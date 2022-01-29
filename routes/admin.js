97b67edb0734c5f8840a1c05bb5fa7cc8517411b
Darren Nienaber <staysignal@outlook.com>
2022-01-29 12:04:36 +0200

    updates to CRUD

--- routes/admin.js
+++ routes/admin.js
@@ -10,5 +10,7 @@ const router = express.Router();
 router.get('/add-product', adminController.getAddProduct);
 router.get('/products', adminController.getProducts);
 router.post('/add-product', adminController.postAddProduct);
+router.get('/edit-product/:id', adminController.getEditProduct);
+router.post('/edit-product', adminController.postEditProducts);
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,9 @@ const router = express.Router();
 // ROUTES
 router.get('/', shopController.getIndex);
 router.get('/products', shopController.getProducts);
+router.get('/products/:id', shopController.getProductById);
 router.get('/cart', shopController.getCart);
+router.post('/cart', shopController.postCart);
 router.get('/orders', shopController.getOrders);
 router.get('/checkout', shopController.getCheckout);
 

0bf8fff89f0ee10d098f954326fad97e5c24cd44
Darren Nienaber <staysignal@outlook.com>
2022-02-03 11:57:16 +0200

    app fully integrated into mongoose

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 // IMPORTING MODELS
 const User = require('./models/user');
 
@@ -16,15 +17,15 @@ app.set('views', 'views');
 //IMPORTING ROUTES
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const { getMaxListeners } = require('process');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ADDING THE USER TO OUR REQUEST OBJECT
 app.use((req, res, next) => {
-    User.findById('61faab3c681a710f5676f0e7')
+    User.findById('61fb8ca07ead0c42705bc0c3')
         .then(user => {
-            req.user = new User(user.name, user.email, user.cart, user._id);
+            req.user = user;
             next();
         })
         .catch(err => console.log(err));
@@ -36,6 +37,21 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-    app.listen(3000);
-})
+mongoose.connect('mongodb+srv://darren-user:jpmpNIJHLvEA0UVu@cluster0.2yyh4.mongodb.net/shopping_cart_node?retryWrites=true&w=majority')
+    .then(result => {
+        User.findOne()
+            .then(user => {
+                if (!user) {
+                    const user = new User({
+                        name: 'Darren',
+                        email: 'test@gmail.com',
+                        cart: {
+                            items: [],
+                        }
+                    });
+                    user.save();
+                }
+            })
+        app.listen(3000);
+    })
+    .catch(err => console.log(err));
--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,6 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-
 router.get('/add-product', adminController.getAddProduct);
 router.get('/products', adminController.getProducts);
 router.post('/add-product', adminController.postAddProduct);

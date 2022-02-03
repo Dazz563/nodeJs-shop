23e757804228488dfc8a5edd8a97395b5ff8f1a8
Darren Nienaber <staysignal@outlook.com>
2022-02-03 07:44:31 +0200

    updates to mongo CRUD

--- app.js
+++ app.js
@@ -24,11 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('61faab3c681a710f5676f0e7')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
-    next();
 });
 
 // USING ROUTES

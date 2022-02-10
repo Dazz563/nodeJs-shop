15bde1da03d697fa4b3ffe9b1595e0da3b8a190b
Darren Nienaber <staysignal@outlook.com>
2022-02-10 08:06:43 +0200

    stripe payments added

--- routes/shop.js
+++ routes/shop.js
@@ -14,7 +14,11 @@ router.get('/products/:productId', shopController.getProduct);
 router.get('/cart', isAuth, shopController.getCart);
 router.post('/cart', isAuth, shopController.postCart);
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
-router.post('/create-order', isAuth, shopController.postOrder);
+// STRIPE
+router.get('/checkout', isAuth, shopController.getCheckout);
+router.get('/checkout/success', isAuth, shopController.getCheckoutSuccess);
+router.get('/checkout/cancel', isAuth, shopController.getCheckout);
+
 router.get('/orders', isAuth, shopController.getOrders);
 router.get('/orders/:orderId', isAuth, shopController.getInvoice);
 
